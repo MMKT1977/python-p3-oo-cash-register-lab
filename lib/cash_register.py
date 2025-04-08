3f789c5c31203b00c0e5c2c134926c838fb9055b
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-04-08 18:31:44 +0300

    initialized, add item and apply discount methods implemented

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount=0):
+    self.total = 0
+    self.discount = discount
+    self.items=[]
+  
+  def add_item(self, title, price, quantity=1):
+    self.total += price * quantity
+    for i in range(quantity):
+      self.items.append(title)
+  
+  def apply_discount(self):
+    if self.discount >0:
+      discount_price = self.total * self.discount /100
+      self.total -= discount_price
+      print(f"After the discount, the total comes to ${int(self.total)}.")
+    else:
+      print("There is no discount to apply.")
+
+new_register = CashRegister(10)
+
+new_register.add_item("eggs", 0.98)
+new_register.add_item("book", 5.00, 3)
+new_register.apply_discount()
+print(f"{new_register.total}")
+print(f"{new_register.items}")
